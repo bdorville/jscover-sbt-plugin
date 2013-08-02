b5f6c3d0660c6759273e12c3fd8f1cc8bd0c35c3
Bastien Dorville <bastien.dorville@gmail.com>
2013-08-02 11:26:59 +0200

    Use the parent folder when there is only a single report

--- jscover-play-plugin/src/main/scala/sbt/JSCoverPlugin.scala
+++ jscover-play-plugin/src/main/scala/sbt/JSCoverPlugin.scala
@@ -121,7 +121,7 @@ object JSCoverPlugin extends Plugin {
 
       reportDirs.length match {
         case 0 => (project.base / "target/test-reports/jscoverReport").absolutePath
-        case 1 => jscoverPrepareSingleReport(project.base / "target/test-reports/jscover", project.base / "target/test-reports/jscoverReport").absolutePath
+        case 1 => jscoverPrepareSingleReport(project.base / "target/test-reports/jscover", project.base / "target/test-reports/jscoverReport").getParentFile.absolutePath
         case _ => deps.seq.foreach{ f =>
             f.map { file =>
               (project.base / "target/test-reports/jscoverReport").absolutePath
